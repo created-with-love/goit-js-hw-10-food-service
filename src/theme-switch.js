aaee3ca0c85502916e131075ebe2f5fcad9f303c
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-10-06 21:53:19 +0300

    добавляет готовый проект

---
+++ src/theme-switch.js
@@ -0,0 +1,41 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// деструктуризирую свойства обьекта для удобства исп-я в будущем
+const { LIGHT, DARK } = Theme;
+
+// выношу класлист body в переменную для удобства
+const bodyStyle = document.body.classList;
+
+const themeSwitchBtn = document.querySelector('#theme-switch-toggle');
+themeSwitchBtn.addEventListener('change', onSwitchThemeHandler);
+
+const storageTheme = localStorage.getItem('theme');
+// storageTheme ?? bodyStyle.add(storageTheme);
+
+// проверяю локальное хранилище на наличие темы - если есть, то устанавливаю. Так же, если тема темная - переключаю чекбокс
+if (storageTheme) {
+  bodyStyle.add(storageTheme);
+  if (bodyStyle.contains(DARK)) {
+    themeSwitchBtn.setAttribute('checked', true);
+  }
+}
+
+if (!bodyStyle.contains(LIGHT) && !bodyStyle.contains(DARK)) {
+  // тема по умолчанию светлая
+  bodyStyle.add(LIGHT);
+}
+
+// функция-переключатель
+function onSwitchThemeHandler(evt) {
+  if (bodyStyle.contains(LIGHT) || bodyStyle.contains(DARK)) {
+    bodyStyle.toggle(DARK);
+    bodyStyle.toggle(LIGHT);
+
+    bodyStyle.contains(LIGHT)
+      ? localStorage.setItem('theme', LIGHT)
+      : localStorage.setItem('theme', DARK);
+  }
+}
